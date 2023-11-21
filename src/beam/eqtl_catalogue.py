c3c409f971a391d5489454e2933f4987896bc019
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-11-21 15:22:46 +0000

    feat: main record processing and partitioning logic

--- src/beam/eqtl_catalogue.py
+++ src/beam/eqtl_catalogue.py
@@ -2,14 +2,15 @@
 
 from __future__ import annotations
 
-import urllib.request
-from io import BytesIO
 from typing import Any, Dict, List
 
 import apache_beam as beam
 import pandas as pd
 
 EQTL_CATALOGUE_IMPORTED_PATH = "https://raw.githubusercontent.com/eQTL-Catalogue/eQTL-Catalogue-resources/master/tabix/tabix_ftp_paths_imported.tsv"
+EQTL_CATALOGUE_OUPUT_BASE = (
+    "gs://genetics_etl_python_playground/1-smart-mirror/summary_stats"
+)
 
 
 def get_input_files() -> List[Dict[str, Any]]:
@@ -18,33 +19,39 @@ def get_input_files() -> List[Dict[str, Any]]:
     Returns:
         List[Dict[str, Any]]: list of input file attribute dictionaries.
     """
-    with urllib.request.urlopen(EQTL_CATALOGUE_IMPORTED_PATH) as response:
-        assert response.getcode() == 200
-        content_bytes = BytesIO(response.read())
-        df = pd.read_csv(content_bytes, sep="\t")
-        return df.to_dict(orient="records")
+    df = pd.read_table(EQTL_CATALOGUE_IMPORTED_PATH)
+    return df.to_dict(orient="records")
 
 
-class ProcessFile(beam.DoFn):
-    """Process one file."""
+class ProcessRecord(beam.DoFn):
+    """Process one input record."""
 
-    def process(self, element: Dict[str, Any]) -> None:
+    def process(self, record: Dict[str, Any]) -> None:
         """Read one file, transform, save as Parquet.
 
         Args:
-            element (Dict[str, Any]): Attribute dictionary for one file.
+            record (Dict[str, Any]): Attribute dictionary for one file.
         """
-        print(element)
+        # Read the file specified by the record.
+        assert (
+            record["study"] == "GTEx_V8"
+        ), "Only GTEx_V8 studies are currently supported."
+        df = pd.read_table(record["ftp_path"])
+        # Output into Parquet, partitioning by chromosome.
+        output_filename = (
+            f"{EQTL_CATALOGUE_OUPUT_BASE}/"
+            "analysisType=eQTL/"
+            "sourceId=eQTL_Catalogue/"
+            f"projectId={record['study']}/"  # Example: "GTEx_V8".
+            f"studyId={record['qtl_group']}/"  # Example: "Adipose_Subcutaneous".
+        )
+        df.to_parquet(output_filename, partition_cols=["chromosome"])
 
 
 def run_pipeline() -> None:
     """Define and run the Apache Beam pipeline."""
     with beam.Pipeline() as pipeline:
-        (
-            pipeline
-            | "Create" >> beam.Create(get_input_files())
-            | "ProcessFiles" >> beam.ParDo(ProcessFile())
-        )
+        (pipeline | beam.Create(get_input_files()) | beam.ParDo(ProcessRecord()))
 
 
 if __name__ == "__main__":
