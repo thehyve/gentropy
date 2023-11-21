7cab5395e69ac940d0b961395e133ae879662a09
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-11-21 15:04:55 +0000

    feat: fetch input dataframe from eQTL Catalogue

--- src/beam/eqtl_catalogue.py
+++ src/beam/eqtl_catalogue.py
@@ -2,31 +2,39 @@
 
 from __future__ import annotations
 
-from typing import List
+import urllib.request
+from io import BytesIO
+from typing import Any, Dict, List
 
 import apache_beam as beam
+import pandas as pd
+
+EQTL_CATALOGUE_IMPORTED_PATH = "https://raw.githubusercontent.com/eQTL-Catalogue/eQTL-Catalogue-resources/master/tabix/tabix_ftp_paths_imported.tsv"
 
 
-def get_input_files() -> List[str]:
-    """Generate the list of input files.
+def get_input_files() -> List[Dict[str, Any]]:
+    """Generate the list of input records.
 
     Returns:
-        List[str]: list of input files.
+        List[Dict[str, Any]]: list of input file attribute dictionaries.
     """
-    return ["file1.txt", "file2.txt", "file3.txt"]
+    with urllib.request.urlopen(EQTL_CATALOGUE_IMPORTED_PATH) as response:
+        assert response.getcode() == 200
+        content_bytes = BytesIO(response.read())
+        df = pd.read_csv(content_bytes, sep="\t")
+        return df.to_dict(orient="records")
 
 
 class ProcessFile(beam.DoFn):
     """Process one file."""
 
-    def process(self, element: str) -> None:
+    def process(self, element: Dict[str, Any]) -> None:
         """Read one file, transform, save as Parquet.
 
         Args:
-            element (str): URI for one input file.
+            element (Dict[str, Any]): Attribute dictionary for one file.
         """
-        with open(f"{element}.output.txt", "w") as f:
-            f.write("Test\n")
+        print(element)
 
 
 def run_pipeline() -> None:
