8328c2581c8275952f2ca6fb9f5f38fbed03eb9f
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-11-22 10:34:49 +0000

    feat: add a non-blocking line-by-line remote TSV iterator

--- src/beam/eqtl_catalogue.py
+++ src/beam/eqtl_catalogue.py
@@ -2,7 +2,11 @@
 
 from __future__ import annotations
 
-from typing import Any, Dict, List
+import gzip
+import io
+import typing
+import urllib.request
+from typing import IO, Any, Dict, Iterator, List
 
 import apache_beam as beam
 import pandas as pd
@@ -13,18 +17,6 @@ EQTL_CATALOGUE_IMPORTED_PATH = "https://raw.githubusercontent.com/eQTL-Catalogue
 EQTL_CATALOGUE_OUPUT_BASE = (
     "gs://genetics_etl_python_playground/1-smart-mirror/summary_stats"
 )
-
-
-def get_input_files() -> List[Dict[str, Any]]:
-    """Generate the list of input records.
-
-    Returns:
-        List[Dict[str, Any]]: list of input file attribute dictionaries.
-    """
-    df = pd.read_table(EQTL_CATALOGUE_IMPORTED_PATH)
-    return df.to_dict(orient="records")
-
-
 FIELDS = [
     "variant",
     "r2",
@@ -50,6 +42,37 @@ PYARROW_SCHEMA = pyarrow.schema(
 )
 
 
+def get_input_files() -> List[Dict[str, Any]]:
+    """Generate the list of input records.
+
+    Returns:
+        List[Dict[str, Any]]: list of input file attribute dictionaries.
+    """
+    df = pd.read_table(EQTL_CATALOGUE_IMPORTED_PATH)
+    return df.to_dict(orient="records")
+
+
+def yield_tsv_from_gzipped_url(url: str) -> Iterator[List[str]]:
+    """A non-blocking line-by-line iterator from a remote source.
+
+    Args:
+        url (str): URL to read the data from.
+
+    Yields:
+        List[str]: List of field values for each row.
+    """
+    with urllib.request.urlopen(url) as compressed_stream:
+        with gzip.GzipFile(fileobj=compressed_stream) as uncompressed_stream:
+            # See: https://stackoverflow.com/a/58407810.
+            typed_stream = typing.cast(IO[bytes], uncompressed_stream)
+            with io.TextIOWrapper(typed_stream) as text_stream:
+                for i, line in enumerate(text_stream):
+                    if i == 0:
+                        # Skip header.
+                        continue
+                    yield line.strip().split("\t")
+
+
 class ProcessRecord(beam.DoFn):
     """Process one input record."""
 
@@ -59,13 +82,14 @@ class ProcessRecord(beam.DoFn):
         Args:
             record (Dict[str, Any]): Attribute dictionary for one file.
         """
-        # Required for correct processing in Dataflow.
-        import pandas as pd
-
         # Read the file specified by the record.
         assert (
             record["study"] == "GTEx_V8"
         ), "Only GTEx_V8 studies are currently supported."
+
+        # Required for correct processing in Dataflow.
+        import pandas as pd
+
         http_path = record["ftp_path"].replace("ftp://", "http://")
         df = pd.read_table(http_path)
         # Output into Parquet, partitioning by chromosome.
