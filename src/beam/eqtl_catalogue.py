78833060661f0bef2d2cc751ceb2240855abfb12
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-11-22 12:04:38 +0000

    feat: embed the parsing iterator into the pipeline

--- src/beam/eqtl_catalogue.py
+++ src/beam/eqtl_catalogue.py
@@ -52,25 +52,35 @@ def get_input_files() -> List[Dict[str, Any]]:
     return df.to_dict(orient="records")
 
 
-def yield_tsv_from_gzipped_url(url: str) -> Iterator[List[str]]:
-    """A non-blocking line-by-line iterator from a remote source.
+class ParseData(beam.DoFn):
+    """Process one input file and yield parsed rows."""
 
-    Args:
-        url (str): URL to read the data from.
+    def process(self, record: Dict[str, Any]) -> Iterator[List[str]]:
+        """A non-blocking line-by-line iterator from a remote source.
 
-    Yields:
-        List[str]: List of field values for each row.
-    """
-    with urllib.request.urlopen(url) as compressed_stream:
-        with gzip.GzipFile(fileobj=compressed_stream) as uncompressed_stream:
-            # See: https://stackoverflow.com/a/58407810.
-            typed_stream = typing.cast(IO[bytes], uncompressed_stream)
-            with io.TextIOWrapper(typed_stream) as text_stream:
-                for i, line in enumerate(text_stream):
-                    if i == 0:
-                        # Skip header.
-                        continue
-                    yield line.strip().split("\t")
+        Args:
+            record (Dict[str, Any]): A record describing one input file and its attributes.
+
+        Yields:
+            List[str]: List of field values for each row.
+        """
+        assert (
+            record["study"] == "GTEx_V8"
+        ), "Only GTEx_V8 studies are currently supported."
+        http_path = record["ftp_path"].replace("ftp://", "http://")
+        with urllib.request.urlopen(http_path) as compressed_stream:
+            with gzip.GzipFile(fileobj=compressed_stream) as uncompressed_stream:
+                # See: https://stackoverflow.com/a/58407810.
+                typed_stream = typing.cast(IO[bytes], uncompressed_stream)
+                with io.TextIOWrapper(typed_stream) as text_stream:
+                    for i, line in enumerate(text_stream):
+                        if i == 0:
+                            # Skip header.
+                            continue
+                        if i == 5:
+                            break
+                        print(line)
+                        yield line.strip().split("\t")
 
 
 class ProcessRecord(beam.DoFn):
@@ -82,16 +92,6 @@ class ProcessRecord(beam.DoFn):
         Args:
             record (Dict[str, Any]): Attribute dictionary for one file.
         """
-        # Read the file specified by the record.
-        assert (
-            record["study"] == "GTEx_V8"
-        ), "Only GTEx_V8 studies are currently supported."
-
-        # Required for correct processing in Dataflow.
-        import pandas as pd
-
-        http_path = record["ftp_path"].replace("ftp://", "http://")
-        df = pd.read_table(http_path)
         # Output into Parquet, partitioning by chromosome.
         output_filename = (
             f"{EQTL_CATALOGUE_OUPUT_BASE}/"
@@ -100,13 +100,19 @@ class ProcessRecord(beam.DoFn):
             f"projectId={record['study']}/"  # Example: "GTEx_V8".
             f"studyId={record['qtl_group']}/"  # Example: "Adipose_Subcutaneous".
         )
-        df.to_parquet(output_filename, partition_cols=["chromosome"])
+        print(output_filename)
+        # df.to_parquet(output_filename, partition_cols=["chromosome"])
 
 
 def run_pipeline() -> None:
     """Define and run the Apache Beam pipeline."""
     with beam.Pipeline(options=PipelineOptions()) as pipeline:
-        (pipeline | beam.Create(get_input_files()) | beam.ParDo(ProcessRecord()))
+        (
+            pipeline
+            | beam.Create(get_input_files()[:2])
+            | beam.ParDo(ParseData())
+            # | beam.ParDo(ProcessRecord())
+        )
 
 
 if __name__ == "__main__":
