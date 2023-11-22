cffea73f2295cda9a22860204523b577ff8790f5
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-11-22 15:09:50 +0000

    feat: update code and parameters for Dataflow run

--- src/beam/eqtl_catalogue.py
+++ src/beam/eqtl_catalogue.py
@@ -2,16 +2,11 @@
 
 from __future__ import annotations
 
-import gzip
-import io
-import typing
-import urllib.request
-from typing import IO, Any, Dict, Iterator, List
+from typing import Any, Dict, Iterator, List
 
 import apache_beam as beam
 import pandas as pd
 import pyarrow
-from apache_beam.io import WriteToParquet
 from apache_beam.options.pipeline_options import PipelineOptions
 
 EQTL_CATALOGUE_IMPORTED_PATH = "https://raw.githubusercontent.com/eQTL-Catalogue/eQTL-Catalogue-resources/master/tabix/tabix_ftp_paths_imported.tsv"
@@ -56,8 +51,11 @@ def get_input_files() -> List[Dict[str, Any]]:
 class ParseData(beam.DoFn):
     """Process one input file and yield parsed rows."""
 
+    FIELDS = FIELDS
+
     def process(
-        self, record: Dict[str, Any]
+        self,
+        record: Dict[str, Any],
     ) -> Iterator[tuple[tuple[str, str], dict[str, str]]]:
         """A non-blocking line-by-line iterator from a remote source.
 
@@ -67,6 +65,11 @@ class ParseData(beam.DoFn):
         Yields:
             tuple[tuple[str, str], dict[str, str]]: QTL group and record dictionary.
         """
+        import gzip
+        import io
+        import typing
+        import urllib.request
+
         assert (
             record["study"] == "GTEx_V8"
         ), "Only GTEx_V8 studies are currently supported."
@@ -74,15 +77,17 @@ class ParseData(beam.DoFn):
         with urllib.request.urlopen(http_path) as compressed_stream:
             with gzip.GzipFile(fileobj=compressed_stream) as uncompressed_stream:
                 # See: https://stackoverflow.com/a/58407810.
-                typed_stream = typing.cast(IO[bytes], uncompressed_stream)
+                typed_stream = typing.cast(typing.IO[bytes], uncompressed_stream)
                 with io.TextIOWrapper(typed_stream) as text_stream:
                     for i, line in enumerate(text_stream):
                         if i == 0:
                             # Skip header.
                             continue
-                        if i == 5:
+                        if i == 1000000:
                             break
-                        data = dict(zip(FIELDS, line.strip().split("\t"), strict=True))
+                        data = dict(
+                            zip(self.FIELDS, line.strip().split("\t"), strict=True)
+                        )
                         chromosome = data["chromosome"]
                         # del data["chromosome"]
                         yield (
@@ -94,8 +99,11 @@ class ParseData(beam.DoFn):
                         )
 
 
-class WriteParquet(beam.DoFn):
+class WriteData(beam.DoFn):
     """Write a record to Parquet format."""
+
+    EQTL_CATALOGUE_OUPUT_BASE = EQTL_CATALOGUE_OUPUT_BASE
+    PYARROW_SCHEMA = PYARROW_SCHEMA
 
     def process(self, element: tuple[Any, Any]) -> None:
         """Write a Parquet file for a given input file.
@@ -103,33 +111,23 @@ class WriteParquet(beam.DoFn):
         Args:
             element (tuple[Any, Any]): key and grouped values.
         """
+        from apache_beam.io import WriteToParquet
+
         (qtl_group, chromosome), records = element
-        records | WriteToParquet(
-            file_path_prefix=f"output_test_{qtl_group}_{chromosome}",
-            file_name_suffix=".parquet",
-            schema=PYARROW_SCHEMA,
-        )
-
-
-class ProcessRecord(beam.DoFn):
-    """Process one input record."""
-
-    def process(self, record: Dict[str, Any]) -> None:
-        """Read one file, transform, save as Parquet.
-
-        Args:
-            record (Dict[str, Any]): Attribute dictionary for one file.
-        """
-        # Output into Parquet, partitioning by chromosome.
         output_filename = (
-            f"{EQTL_CATALOGUE_OUPUT_BASE}/"
+            f"{self.EQTL_CATALOGUE_OUPUT_BASE}/"
             "analysisType=eQTL/"
             "sourceId=eQTL_Catalogue/"
-            f"projectId={record['study']}/"  # Example: "GTEx_V8".
-            f"studyId={record['qtl_group']}/"  # Example: "Adipose_Subcutaneous".
+            "projectId=GTEx_V8/"
+            f"studyId={qtl_group}/"  # Example: "Adipose_Subcutaneous".
+            f"chromosome={chromosome}/"  # Example: 13.
+            "part"
         )
-        print(output_filename)
-        # df.to_parquet(output_filename, partition_cols=["chromosome"])
+        records | WriteToParquet(
+            file_path_prefix=output_filename,
+            file_name_suffix=".parquet",
+            schema=self.PYARROW_SCHEMA,
+        )
 
 
 def run_pipeline() -> None:
@@ -137,16 +135,10 @@ def run_pipeline() -> None:
     with beam.Pipeline(options=PipelineOptions()) as pipeline:
         (
             pipeline
-            | "Prepare list of input files" >> beam.Create(get_input_files()[:2])
-            | "Fetch, uncompress, and parse data" >> beam.ParDo(ParseData())
-            | "Group the data by input file" >> beam.GroupByKey()
-            | "Write to Parquet" >> beam.ParDo(WriteParquet())
-            # >> WriteToParquet(
-            #     file_path_prefix="output_test",
-            #     file_name_suffix=".parquet",
-            #     schema=PYARROW_SCHEMA,
-            # )
-            # | beam.ParDo(ProcessRecord())
+            | "List input files" >> beam.Create(get_input_files())
+            | "Parse data" >> beam.ParDo(ParseData())
+            | "Aggregate data" >> beam.GroupByKey()
+            | "Write to Parquet" >> beam.ParDo(WriteData())
         )
 
 
